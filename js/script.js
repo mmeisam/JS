a10216187f326a3ea370fbb9807129e4819c76a1
Meisam Masoudi <69512724+mmeisam@users.noreply.github.com>
2024-03-14 09:14:31 +0330

    Add files via upload
    37-keycode

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+let ascii = $.querySelector('#ascii')
+let key = $.querySelector('#key')
+let keyLocation = $.querySelector('#location')
+let keywhich = $.querySelector('#which')
+let keycode = $.querySelector('#code')
+
+document.body.addEventListener('keydown', function (event) {
+	event.preventDefault();
+	console.log(event);
+	ascii.innerHTML = event.keyCode;
+	key.innerHTML = event.key;
+	keyLocation.innerHTML = event.location;
+	keywhich.innerHTML = event.which;
+	keycode.innerHTML = event.code;
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+})
